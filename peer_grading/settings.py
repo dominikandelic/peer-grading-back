895fb66a87457c7946f9f4fa30532fefa140fd3b
dominikandelic <dominik.andelic@em2.hr>
2023-06-09 15:12:02 +0200

    Add cors whitelist, requirements

--- peer_grading/settings.py
+++ peer_grading/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     "peer_grading",
     "ninja_jwt",
     "ninja_extra",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -68,6 +70,11 @@ TEMPLATES = [
             ],
         },
     },
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
 
 WSGI_APPLICATION = "peer_grading.wsgi.application"
